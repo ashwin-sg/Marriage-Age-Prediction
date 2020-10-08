87296a3a670f0667d884e96c9da7729aa9651c21
ashwin-sg <65702360+ashwin-sg@users.noreply.github.com>
2020-10-08 08:49:53 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ from flask_cors import CORS
 CORS(app)
 
 # main index page route
-@app.route('/')
-def home():
-    return '<h1>API is working.. </h1>'
+# @app.route('/')
+# def home():
+#     return '<h1>API is working.. </h1>'
 
 
-@app.route('/predict',methods=['GET'])
+@app.route('/',methods=['GET'])
 def predict():
     import joblib
     model = joblib.load('marriage_age_predict_model.ml')
